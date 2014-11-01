635776ed8ebf85fccd357e5a3872cbab561c73d5
twinaphex <libretro@gmail.com>
2014-11-02 00:54:47 +0100

    Add preliminary JNI Makefile

--- Src/Libretro/Glob.c
+++ Src/Libretro/Glob.c
@@ -109,7 +109,7 @@ void archGlobFree(ArchGlob* globHandle)
 
 #else
 #include "ArchGlob.h"
-#if defined(PSP) || defined(__CELLOS_LV2__)
+#if defined(PSP) || defined(__CELLOS_LV2__) || defined(ANDROID)
 /* TODO/FIXME - might want to turn this into more generic define that we
  * flick on for consoles/portables */
 #include "psp/diet-glob.h"
