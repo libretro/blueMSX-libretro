13fc96bd5c67cf92eee9c4c70e2de3f6079440d0
twinaphex <libretro@gmail.com>
2017-07-04 07:43:45 +0200

    Add Win32 code to Src/Libretro/Timer.c

--- Src/Libretro/Timer.c
+++ Src/Libretro/Timer.c
@@ -1,5 +1,9 @@
 #include "ArchTimer.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #ifdef PSP
 //#include <pspkernel.h>
 #include <psptypes.h>
@@ -13,6 +17,27 @@ UInt32 archGetSystemUpTime(UInt32 frequency)
    return (rtc_tick / frequency);
 }
 
+#elif defined(_WIN32)
+static LONGLONG uptime_hfFrequency = 0;
+static signed long long uptime_offset = 0;
+
+UInt32 archGetSystemUpTime(UInt32 frequency)
+{
+   LARGE_INTEGER li;
+
+   if (!uptime_hfFrequency) {
+      if (QueryPerformanceFrequency(&li)) {
+         uptime_hfFrequency = li.QuadPart;
+      }
+      else {
+         return 0;
+      }
+   }
+
+   QueryPerformanceCounter(&li);
+
+   return (DWORD)((li.QuadPart + uptime_offset) * frequency / uptime_hfFrequency);
+}
 #else
 
 #include <stdlib.h>
