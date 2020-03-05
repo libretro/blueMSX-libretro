0ba1db41422a13ff5c7968d7922d087591e0986f
Vladimir Serbinenko <phcoder@gmail.com>
2020-03-05 17:30:10 +0100

    Fix 3DS compilation

--- Src/Libretro/Glob.c
+++ Src/Libretro/Glob.c
@@ -109,7 +109,7 @@ void archGlobFree(ArchGlob* globHandle)
 
 #else
 #include "ArchGlob.h"
-#if defined(PSP) || defined(__CELLOS_LV2__) || defined(ANDROID) || defined(WIIU) || defined(_XBOX) || defined(VITA)
+#if defined(PSP) || defined(__CELLOS_LV2__) || defined(ANDROID) || defined(WIIU) || defined(_XBOX) || defined(VITA) || defined(_3DS)
 /* TODO/FIXME - might want to turn this into more generic define that we
  * flick on for consoles/portables */
 #include "psp/diet-glob.h"
